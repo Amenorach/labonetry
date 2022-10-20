5a623995429a6be2c4033d7a71d6ede58a6f70b7
Amenorach <zachery.annancy@ashesi.edu.gh>
2022-10-20 21:09:07 +0000

    Update dbConnectCredentials.php

--- Settings/dbConnectCredentials.php
+++ Settings/dbConnectCredentials.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME','root');
-define('DB_PASSWORD','');
-define('DB_NAME', 'shoppn');
+define('DB_USERNAME','id19736102_shoppnn');
+define('DB_PASSWORD','WFi<(\m6vUIFX5O_');
+define('DB_NAME', 'id19736102_shoppn');
 ?>
